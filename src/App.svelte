aed60c12b548833f264001a58d21b8d66ce650c1
Vince Juliano <v.juliano6363@gmail.com>
2023-02-16 13:39:19 -0500

    add leaderboard route

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import { Route, router } from "tinro";
 
+  import Leaderboard from "./leaderboard.svelte";
+
   // setup hash router mode
   router.mode.hash();
 </script>
 
 <Route path="/">
-  <div class="hero min-h-screen">
-    <div class="hero-content">
-      <h1 class="text-6xl">SWAG Game</h1>
-    </div>
-  </div>
+  <Leaderboard />
 </Route>
---
+++ src/leaderboard.svelte
@@ -0,0 +1,7 @@
+
+
+<div class="hero min-h-screen">
+    <div class="hero-content">
+        <h1 class="text-6xl">Leaderboard View</h1>
+    </div>
+</div>
