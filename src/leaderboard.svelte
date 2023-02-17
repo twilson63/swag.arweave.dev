8dc3b232d044f6aba9bb380916ef494965e71311
Vince Juliano <v.juliano6363@gmail.com>
2023-02-17 11:26:46 -0500

    merge with main

--- src/leaderboard.svelte
+++ src/leaderboard.svelte
@@ -2,11 +2,33 @@
   import { getContext } from "svelte";
 
   const { leaderboard } = getContext("data");
+
+  async function getTopThree() {
+    let leaders = await leaderboard();
+    let topThree = [];
+    for(let i = 0; i < 3; i++){
+      topThree.push(leaders[i]);
+    }
+    return topThree;
+  }
+
 </script>
 
-{#await leaderboard() then players}
-  {#each players as player}
-    <div>Leaderboard view</div>
-    <pre>{JSON.stringify(player, null, 2)}</pre>
-  {/each}
+{#await getTopThree() then topThree}
+  <div class="avatar">
+    <div class="w-24 rounded-xl">
+      <img src="{topThree[0].avatarURL}" />
+    </div>
+  </div>
+  <div class="avatar">
+    <div class="w-24 rounded-xl">
+      <img src="{topThree[0].avatarURL}" />
+    </div>
+  </div>
+  <div class="avatar">
+    <div class="w-24 rounded-xl">
+      <img src="{topThree[0].avatarURL}" />
+    </div>
+  </div>
 {/await}
+
