7339de34c77bcabc822702a843927e0ba6d4136b
Tom Wilson <tom@hyper.io>
2023-02-18 16:41:58 +0000

    chore: fixed stampjs dependency

---
+++ src/services/stamp-utils.js
@@ -0,0 +1,19 @@
+import Stamps from 'https://esm.sh/@permaweb/stampjs'
+import { prop } from 'ramda'
+
+const { WarpFactory } = window.warp
+
+const warp = WarpFactory.forMainnet()
+const stamps = Stamps.init({ warp })
+
+export function stamp(tx) {
+  return stamps.stamp(tx)
+}
+
+export function count(tx) {
+  return stamps.count(tx).then(prop('total'))
+}
+
+export function filter(logic) {
+  return []
+}
---
+++ src/services/stamp-utils.test.js
@@ -0,0 +1,14 @@
+import { assert } from 'https://deno.land/std/testing/asserts.ts'
+import warp from 'warp-contracts'
+
+warp.LoggerFactory.INST.logLevel('fatal')
+window.warp = warp
+
+const { test } = Deno
+
+test('Get Stamp Count', async () => {
+  const { count } = (await import('./stamp-utils.js'))
+
+  const result = await count('M4sru2azaPEY-zOViXhCtfygFpvBy7C0ir8iOnrgRdA')
+  assert(result === 25)
+})
