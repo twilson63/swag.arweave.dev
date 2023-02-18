4a082ec1f682b5f56792013814fcd0beba2fd3ae
mogulx_operates <mogulx_operates@yahoo.com>
2023-02-18 13:37:30 -0600

    feat: balance func

--- src/services/stamp-utils.js
+++ src/services/stamp-utils.js
@@ -1,19 +1,23 @@
-import Stamps from 'https://esm.sh/@permaweb/stampjs'
-import { prop } from 'ramda'
+import Stamps from "https://esm.sh/@permaweb/stampjs";
+import { prop } from "ramda";
 
-const { WarpFactory } = window.warp
+const { WarpFactory } = window.warp || (await import("warp-contracts")).default;
 
-const warp = WarpFactory.forMainnet()
-const stamps = Stamps.init({ warp })
+const warp = WarpFactory.forMainnet();
+const stamps = Stamps.init({ warp });
 
 export function stamp(tx) {
-  return stamps.stamp(tx)
+  return stamps.stamp(tx);
 }
 
 export function count(tx) {
-  return stamps.count(tx).then(prop('total'))
+  return stamps.count(tx).then(prop("total"));
 }
 
 export function filter(logic) {
-  return []
+  return [];
+}
+
+export function balance(tx) {
+  return stamps.balance(tx);
 }
