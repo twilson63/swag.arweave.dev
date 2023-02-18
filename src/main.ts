06eb55fa46700380bc8487a87fb984fa2c2da310
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-02-19 04:55:54 +0500

    twind.style configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import "./app.css";
 import App from "./App.svelte";
 import { defineConfig, install } from "https://esm.sh/@twind/core";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind";
 
-install(defineConfig({}));
+install(defineConfig({
+  presets: [presetAutoprefix(), presetTailwind()],
+}));
 
 const app = new App({
   target: document.getElementById("app"),
--- src/splash.svelte
+++ src/splash.svelte
@@ -1,4 +1,6 @@
 <script>
 </script>
 
-<div>Splash Screen</div>
+<section class="">
+    <h1 class="text-3xl font-bold underline">Splash Screen</h1>
+</section>
