87891a597c2d5b0e709da00bf36a9605f38aa485
mogulx_operates <mogulx_operates@yahoo.com>
2023-02-18 13:39:11 -0600

    chore: cleanup unused imports

--- src/lib/player-stamp-stats.js
+++ src/lib/player-stamp-stats.js
@@ -1,8 +1,4 @@
-// import crocks from "crocks";
-// import { compose, pluck, path, head, prop } from "ramda";
 import { balance } from "../services/stamp-utils.js";
-// const { Async, ReaderT } = crocks;
-// const { of, ask, lift } = ReaderT(Async);
 
 export async function playerStampStats(id) {
   const b = await balance(id);
