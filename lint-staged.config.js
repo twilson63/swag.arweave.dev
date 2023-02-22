5ff45cc207054341b4e3d4c3a77adcb8d5f52558
Tyler Hall <tyler@hyper.io>
2023-02-22 14:27:30 +0000

    chore(dev): generate README ToCs and hook into pre-commit #80

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   '**/*.{ts,js,mjs,cjs,md,json,yml}': ['prettier --write --plugin-search-dir=.'],
-  'package.json': ['sort-package-json', 'prettier --write']
+  'package.json': ['sort-package-json', 'prettier --write'],
+  'README.md': ['markdown-toc-gen insert', 'prettier --write']
 };
