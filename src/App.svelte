e1984852fd0b9e281155f8a9df168eec3ce31145
Tom Wilson <tom@hyper.io>
2023-02-25 14:33:21 +0000

    feat: support for arweave tx #117

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import { Route } from "tinro";
   import Leaderboard from "./leaderboard.svelte";
-  // import Leaderboard from "./components/organisms/leaderboard.svelte";
-  // import Player from "./components/organisms/player.svelte";
-  // import Splash from "./components/atoms/splash.svelte";
 
   // setup hash router mode
   // router.mode.hash();
 </script>
 
 <Route path="/" let:meta>
+  <Leaderboard tx={meta.query.tx} qr={meta.query.id} />
+</Route>
+
+<Route path="/:tx" let:meta>
   <Leaderboard tx={meta.query.tx} qr={meta.query.id} />
 </Route>
